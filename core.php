e273f97aa6510b2aeaed135d467df5f0c5d622cc
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-03-03 16:21:51 +0100

    Update core.php

--- core.php
+++ core.php
@@ -4,7 +4,7 @@
  * The plugin bootstrap file
  * @link              https://wordpress-webmaster.de
  * @since             1.0.0
- * @package           SETeam
+ * @package           WPWM
  *
  * @wordpress-plugin
  * Plugin Name:       Wp-Experiment
@@ -21,34 +21,37 @@
 
 class WPAdvancedToolboxPDO {
 	protected $pdo;
+	protected $WPAdvancedToolboxPDO;
+	protected $version;
+	protected $settings_fields;
+	protected $do_settings_sections;
 	private $wp_advanced_toolbox__options;
-
+	//  start function __construct
 	public function __construct() {
-		try {
-			$this->pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4", DB_USER, DB_PASSWORD);
+		try {		$this->pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4", DB_USER, DB_PASSWORD);
 			$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		} catch (PDOException $e) {
 			die("Connection failed: " . $e->getMessage());
-		}
-
-		add_action('admin_menu', array($this, 'wp_advanced_toolbox_add_plugin_page'));
-		add_action('admin_init', array($this, 'wp_advanced_toolbox_page_init'));
 	}
+			// Name & Version for updates
+			$this->plugin_name = 'wp-advanced-toolbox-pdp-stmt';
+			$this->version = '1.0.b';
+			$this->require_once plugin_dir_path( dirname( __FILE__ ) ) . 'register-settings.php';
+			$this->require_once plugin_dir_path( dirname( __FILE__ ) ) . 'register-settings.php';
 
-	private function wp_advanced_toolbox_add_plugin_page() {
-		add_options_page(
-			'Wp-Experiment',
-			'Wp-Experiment',
-			'manage_options',
-			'wp-advanced-toolbox',
-			array($this, 'wp_advanced_toolbox_create_admin_page')
-		);
-	}
-
-	private function wp_advanced_toolbox_create_admin_page() {
-		$this->wp_advanced_toolbox__options = get_option('wp_advanced_toolbox__option_name');
-		?>
-		<div class="wrap">
+			// class optionpage	
+			$plugin_public = new WPAdvancedToolboxPDO( $this->add_settings_field(), 
+								   $this->get_version(), 
+								   $this->do_settings_sections() );
+			$this->add_action('admin_menu', array($this, 'wp_advanced_toolbox_add_plugin_page'));
+			$this->add_action('admin_init', array($this, 'wp_advanced_toolbox_page_init'));
+			$this->wp_advanced_toolbox_add_plugin_page() {
+			$this->add_options_page('Wp-Experiment','Wp-Experiment','manage_options','wp-advanced-toolbox',array($this, 'wp_advanced_toolbox_create_admin_page'));
+			$this->wp_advanced_toolbox_create_admin_page() {
+			$this->wp_advanced_toolbox__options = get_option('wp_advanced_toolbox__option_name');
+			// escape php? dont like!
+			?> 
+			<div class="wrap">
 			<h2>Wp-Experiment</h2>
 			<p>some text 1 some text 2</p>
 			<form method="post" action="options.php">
@@ -57,49 +60,15 @@ class WPAdvancedToolboxPDO {
 				<?php submit_button(); ?>
 			</form>
 		</div>
-		<?php
+		<?php // start php
 	}
-
-	public function wp_advanced_toolbox_page_init() {
-		register_setting(
-			'wp_advanced_toolbox__option_group',
-			'wp_advanced_toolbox__option_name',
-			array($this, 'wp_advanced_toolbox_sanitize')
-		);
-
-		add_settings_section(
-			'wp_advanced_toolbox__setting_section',
-			'Settings',
-			array($this, 'wp_advanced_toolbox_section_info'),
-			'wp-advanced-toolbox-admin'
-		);
-
-		add_settings_field(
-			'rsd_link_0',
-			'RSD link',
-			array($this, 'rsd_link_0_callback'),
-			'wp-advanced-toolbox-admin',
-			'wp_advanced_toolbox__setting_section'
-		);
+		
 	}
-
 	
+	
+	
+	//  end function __construct
 
-	public function wp_advanced_toolbox_sanitize($input) {
-		$sanitary_values = array();
-
-		if (isset($input['rsd_link_0'])) {
-			$sanitary_values['rsd_link_0'] = $input['rsd_link_0'];
-		}
-
-		if (isset($input['rsd_link_0'])) {
-			$new_option_value = $input['rsd_link_0'];
-			$stmt_update_option = $this->pdo->prepare("UPDATE wp_options SET option_value = :rsd_link_0 WHERE option_name = 'remove RSD link'");
-			)
-
-			return $sanitary_values;
-}
-}
 if (is_admin()) {
 $wp_advanced_toolbox = new WPAdvancedToolboxPDO();
 $wp_advanced_toolbox_options = get_option('wp_advanced_toolbox__option_name');
